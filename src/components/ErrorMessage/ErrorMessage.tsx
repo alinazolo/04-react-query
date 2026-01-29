3a6b549d779e7664ee10b9ebf1cb34d814cda430
Alina <alinazolotopup99@gmail.com>
2026-01-30 00:00:37 +0100

    update

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 import css from "./ErrorMessage.module.css";
 
-export default function ErrorMessage() { return (
+export default function ErrorMessage() { 
+    return (
 <p className={css.text}>There was an error, please try again...</p>
+///
 )
-    
 }
